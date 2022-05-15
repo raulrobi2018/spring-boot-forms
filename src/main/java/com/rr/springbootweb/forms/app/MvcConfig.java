bc97337429624dbf51eecb1bdb1f4d4f0e642a03
Raul Rodriguez <raulrobi@gmail.com>
2022-05-14 21:22:11 -0300

    Class 93 - Registering interceptor in the Spring configuration

---
+++ src/main/java/com/rr/springbootweb/forms/app/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.rr.springbootweb.forms.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Autowired
+	@Qualifier("timeElapsedInterceptor")
+	private HandlerInterceptor timeElapsedInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(timeElapsedInterceptor);
+	}
+}
--- src/main/java/com/rr/springbootweb/forms/app/interceptors/TimeElapsedInterceptor.java
+++ src/main/java/com/rr/springbootweb/forms/app/interceptors/TimeElapsedInterceptor.java
@@ -8,10 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-@Component
+@Component("timeElapsedInterceptor")
 public class TimeElapsedInterceptor implements HandlerInterceptor {
 
 	private static final Logger logger = LoggerFactory.getLogger(TimeElapsedInterceptor.class);
@@ -19,7 +20,17 @@ public class TimeElapsedInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		
+		if (handler instanceof HandlerMethod) {
+			//Aquí accedemos al handler y podemos imprimir cualquier dato que este maneje
+			HandlerMethod method = (HandlerMethod) handler;
+			logger.info("This is a controller method " + method.getMethod().getName());
+		}
+		
 		logger.info("TimeElapsedInterceptor preHandle() : loading...");
+		
+		//Esto sirve para saber qué es lo que está interceptando y si da error poder verlo
+		logger.info("Intercepting handler: " + handler);
 		long initialTime = System.currentTimeMillis();
 		request.setAttribute("initialTime", initialTime);
 		Random random = new Random();
@@ -33,13 +44,15 @@ public class TimeElapsedInterceptor implements HandlerInterceptor {
 			ModelAndView modelAndView) throws Exception {
 		long endTime = System.currentTimeMillis();
 		long initialTime = (Long) request.getAttribute("initialTime");
-		long elapsedTime = endTime - initialTime;
+		long timeElapsed = endTime - initialTime;
 
-		if (modelAndView != null) {
-			modelAndView.addObject("elapsedTime", elapsedTime);
+		//Solo con la validación del handler ya bastaría, pero si queremos ser más estrictos
+		//podemos validar también el modelAndView
+		if (handler instanceof HandlerMethod && modelAndView != null) {
+			modelAndView.addObject("timeElapsed", timeElapsed);
 		}
 
-		logger.info("Elapsed time: " + elapsedTime + " miliseconds");
+		logger.info("Time elapsed: " + timeElapsed + " milliseconds");
 		logger.info("TimeElapsedInterceptor postHandle() : loading...");
 	}
 }
